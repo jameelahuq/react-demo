2b6fb655b5e032da991c133900aa7ca857594ef3
Jameela Huq <jameela.huq@gmail.com>
2015-10-06 12:04:29 -0700

    rendered views have header and children

--- public/js/main.js
+++ public/js/main.js
@@ -1,104 +1,55 @@
-/**
- * Created by HUQ on 10/6/15.
- */
-var ProductCategoryRow = React.createClass({
-      render: function() {
-        return (<tr><th colSpan="2">{this.props.category}</th></tr>);
-}
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+        <div className="commentBox">
+          <h1>Comments</h1>
+          <CommentList />
+          <CommentForm />
+        </div>
+    );
+  }
 });
 
-var ProductRow = React.createClass({
-      render: function() {
-        var name = this.props.product.stocked ?
-            this.props.product.name :
-        <span style={{color: 'red'}}>
-      {this.props.product.name}
-    </span>;
-return (
-    <tr>
-    <td>{name}</td>
-    <td>{this.props.product.price}</td>
-</tr>
+var CommentList = React.createClass({
+  render: function() {
+    return (
+        React.createElement('div', {className: "commentList"},
+            <div className="commentList">
+              <Comment author="Jameela Huq">This is the most amazing comment</Comment>
+              <Comment author="Giant Monster">Eat *me* and my *brother*</Comment>
+            </div>)
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+        React.createElement('div', {className: "commentForm"},
+        "Hello, world! I am a CommentForm.")
+    );
+  }
+});
+
+var Comment = React.createClass({
+  rawMarkup: function () {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup}
+  },
+
+  render: function() {
+    return (
+        <div className="comment">
+          <h2 className="commentAuthor">
+            {this.props.author}
+          </h2>
+          <span dangerouslySetInnerHTML={this.rawMarkup()}/>
+        </div>
+    )
+  }
+});
+
+React.render(
+    React.createElement(CommentBox, null),
+    document.getElementById('content')
 );
-}
-});
-
-var ProductTable = React.createClass({
-      render: function() {
-        var rows = [];
-        var lastCategory = null;
-        this.props.products.forEach(function(product) {
-              if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
-                return;
-              }
-              if (product.category !== lastCategory) {
-                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-      }
-      rows.push(<ProductRow product={product} key={product.name} />);
-lastCategory = product.category;
-}.bind(this));
-return (
-    <table>
-    <thead>
-    <tr>
-    <th>Name</th>
-    <th>Price</th>
-    </tr>
-    </thead>
-    <tbody>{rows}</tbody>
-    </table>
-);
-}
-});
-
-var SearchBar = React.createClass({
-    render: function() {
-  return (
-      <form>
-      <input type="text" placeholder="Search..." value={this.props.filterText} />
-<p>
-<input type="checkbox" checked={this.props.inStockOnly} />
-{' '}
-Only show products in stock
-</p>
-</form>
-);
-}
-});
-
-var FilterableProductTable = React.createClass({
-    getInitialState: function() {
-  return {
-    filterText: '',
-    inStockOnly: false
-  };
-},
-
-render: function() {
-  return (
-      <div>
-      <SearchBar
-  filterText={this.state.filterText}
-inStockOnly={this.state.inStockOnly}
-/>
-<ProductTable
-products={this.props.products}
-filterText={this.state.filterText}
-inStockOnly={this.state.inStockOnly}
-/>
-</div>
-);
-}
-});
-
-
-var PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
-
-React.render(<FilterableProductTable products={PRODUCTS} />, document.body);
